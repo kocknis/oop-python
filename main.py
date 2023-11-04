cdc03ce0be112313900b9f1759c981792b4713c0
Sina <bekarsina@gmail.com>
2023-11-04 22:02:54 +0330

    updata about count money for sons

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-  
 # howManyDaughter = int(input('how many daughter does it has : '))
 allMoneysGrandpa = int(input('how mauch grandpa had money : '))
 # isAliveGrandma = input('is alive Grandma : y/n  ')
@@ -23,26 +21,20 @@ class Grandma(Grandpa):
     
     
 class Son:
-    def __init__(self, isMarried, allMoney):
+    def __init__(self, isMarried, allMoney, howManySons):
         self.allMoney = allMoney
         self.isMarried = isMarried
+        self.howManySons = howManySons
     
     def heredity(self):
-        print(self.allMoney)
+        print(self.allMoney / howManySons)
 
-allMoneysGrandpa = int(input('how mauch grandpa had money : '))
-# isAliveGrandma = input('is alive Grandma : y/n  ')
-howManySons = int(input('how many sons does it has : '))
-# isMarriedSons = []
-# for i in range(howManySons):
-#     isMarriedSon = input(f'is Married Son {i + 1} : y/n  ')
-#     isMarriedSons.append(isMarriedSon)
   
 Ali = Grandpa(allMoneysGrandpa)
 soghra = Grandma(True, allMoneysGrandpa)
 sons = []
 for i in range(howManySons):
-    son = Son(False, allMoneysGrandpa)
+    son = Son(False, allMoneysGrandpa, howManySons)
     sons.append(son)
 
 soghra.heredity()
