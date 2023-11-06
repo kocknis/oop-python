13439a43f302fab078323e6578333233eaa2840a
Sina <bekarsina@gmail.com>
2023-11-06 19:45:40 +0330

    updata the share in class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # howManyDaughter = int(input('how many daughter does it has : '))
-allMoneysGrandpa = int(input('how mauch grandpa had money : '))
+allMoneysGrandpa = int(input("how mauch grandpa had money : "))
 # isAliveGrandma = input('is alive Grandma : y/n  ')
-howManySons = int(input('how many sons does it has : '))
+howManySons = int(input("how many sons does it has : "))
 # isMarriedSons = []
 # for i in range(howManySons):
 #     isMarriedSon = input(f'is Married Son {i + 1} : y/n  ')
@@ -10,37 +10,43 @@ howManySons = int(input('how many sons does it has : '))
 sonShare = 16
 daugtherShare = 8
 grandmaShare = 1
+
+
 class Grandpa:
     def __init__(self, allMoney):
         self.allMoney = allMoney
-  
+
+
 class Grandma(Grandpa):
-    def __init__(self, isAlive, allMoney):
+    def __init__(self, isAlive, allMoney, grandmaShare):
         super().__init__(allMoney)
-        self.isAlive = isAlive 
+        self.isAlive = isAlive
 
     def heredity(self):
         print(self.allMoney)
-    
-    
+
+
 class Son:
-    def __init__(self, isMarried, allMoney, howManySons):
+    def __init__(self, isMarried, allMoney, howManySons, sonShare):
         self.allMoney = allMoney
         self.isMarried = isMarried
         self.howManySons = howManySons
-    
+        self.sonShare = sonShare
+
     def heredity(self):
-        print(self.allMoney / howManySons)
-        
+        print(self.allMoney / (howManySons * sonShare))
+
+
 class Daugther:
-    def __init__(self, allMoney, howManyDaugther):
+    def __init__(self, allMoney, howManyDaugther, daugtherShare):
         self.allMoney = allMoney
         self.howManySons = howManySons
-    
+        self.daugtherShare = daugtherShare
+
     def heredity(self):
-        print(self.allMoney / howManySons)
-        
-  
+        print(self.allMoney / (howManySons * daugtherShare))
+
+
 Ali = Grandpa(allMoneysGrandpa)
 soghra = Grandma(True, allMoneysGrandpa)
 sons = []
