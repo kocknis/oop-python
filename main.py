179ef18b3b2af42521868f3be84a5d6d48b73cc1
Sina <bekarsina@gmail.com>
2023-11-09 03:17:08 +0330

    updata

--- main.py
+++ main.py
@@ -2,14 +2,7 @@ allMoneysGrandpa = int(input("how mauch grandpa had money : "))
 isAliveGrandma = input('is alive Grandma : y/n  ')
 howManyDaughter = int(input('how many daughter does it has : '))
 howManySons = int(input("how many sons does it has : "))
-# isMarriedSons = []
-# for i in range(howManySons):
-#     isMarriedSon = input(f'is Married Son {i + 1} : y/n  ')
-#     isMarriedSons.append(isMarriedSon)
 
-# sonShare = 16
-# DaughterShare = 8
-# grandmaShare = 1
 
 allShare = howManySons * 16 + howManyDaughter * 8
 
@@ -31,9 +24,8 @@ class Grandma(Grandpa):
 
 
 class Son:
-    def __init__(self, isMarried, allMoney, howManySons, allShare):
+    def __init__(self, allMoney, howManySons, allShare):
         self.allMoney = allMoney
-        self.isMarried = isMarried
         self.howManySons = howManySons
         self.allShare = allShare
 
@@ -56,7 +48,7 @@ Ali = Grandpa(allMoneysGrandpa)
 if howManySons != 0:
     sons = []
     for i in range(howManySons):
-        son = Son(False, allMoneysGrandpa, howManySons, allShare)
+        son = Son(allMoneysGrandpa, howManySons, allShare)
         sons.append(son)
     
 if howManyDaughter != 0:
